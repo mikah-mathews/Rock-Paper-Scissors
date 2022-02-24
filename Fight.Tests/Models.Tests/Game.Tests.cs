0d1dc34eedab55a8e2ddcaf41873acbd784cb8d1
Mikah Mathews <mikah@outofband.org>
2022-02-24 10:28:52 -0800

    Write comparing input function
    Co-authored-by: Brenna Lavin <lavinbrenna@gmail.com>

--- Fight.Tests/Models.Tests/Game.Tests.cs
+++ Fight.Tests/Models.Tests/Game.Tests.cs
@@ -26,5 +26,19 @@ namespace Fight.Tests
       Assert.AreEqual(player1Input, p1Input);
       Assert.AreEqual(player2Input, p2Input);
     }  
+
+    [TestMethod]
+    public void ComparePlayerInput_ReturnsOutput_String()
+    {
+      string player1Input = "rock";
+      string player2Input = "scissors";
+      Game newGame = new Game(player1Input, player2Input);
+      string result = Game.Compare(player1Input, player2Input);
+      Assert.AreEqual(result, "Player 1 wins!");
+
+    }
+
+
+    
   }
 }
--- Fight/Models/Game.cs
+++ Fight/Models/Game.cs
@@ -9,8 +9,32 @@ namespace Fight.Models
     public string Player2Input{get;set;}
     public Game(string player1Input, string player2Input)
     {
-      Player1Input = player1Input;
-      Player2Input = player2Input;
+      Player1Input = player1Input.ToLower();
+      Player2Input = player2Input.ToLower();
+    }
+
+    public static string Compare(string player1Input, string player2Input)
+    {
+      if ((player1Input == "rock" || player1Input == "scissors" || player1Input == "paper") && (player2Input == "rock" || player2Input == "scissors" || player2Input == "paper"))
+      {
+        if ((player1Input == "rock" && player2Input == "scissors") || (player1Input == "scissors" && player2Input == "paper") || (player1Input == "paper" && player2Input == "rock"))
+        {
+          return "Player 1 wins!";
+        }
+        else if ((player1Input == "rock" && player2Input == "paper") || (player1Input == "scissors" && player2Input == "rock") || (player1Input == "paper" && player2Input == "scissors"))
+        {
+          return "Player 2 wins!";
+        }
+        else
+        {
+          return "It's a tie!";
+        }
+      }
+      else
+      {
+        return "Invalid input";
+      }
+
     }
   }
 }
